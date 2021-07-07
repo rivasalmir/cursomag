d89336b5e3396fd943b43bab94f511e5008fde4c
Almir F Rivas Jr <rivasalmir@gmail.com>
2021-07-07 11:23:15 -0300

    Create table using schema setup

--- app/code/Mageplaza/HelloWorld/Controller/Index/Test.php
+++ app/code/Mageplaza/HelloWorld/Controller/Index/Test.php
@@ -3,15 +3,6 @@ namespace Mageplaza\HelloWorld\Controller\Index;
 
 class Test extends \Magento\Framework\App\Action\Action
 {
-    protected $_pageFactory;
-
-    public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $pageFactory)
-    {
-        $this->_pageFactory = $pageFactory;
-        return parent::__construct($context);
-    }
 
     public function execute()
     {
