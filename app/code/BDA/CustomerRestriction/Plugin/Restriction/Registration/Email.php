0cbf2e68a1fc81054a14ccaa498d38b14eee722b
Almir F Rivas Jr <rivasalmir@gmail.com>
2021-09-13 10:30:58 -0300

    Instalação e customização do módulo BDA/CustomerRestriction

---
+++ app/code/BDA/CustomerRestriction/Plugin/Restriction/Registration/Email.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace BDA\CustomerRestriction\Plugin\Restriction\Registration;
+
+class Email
+{
+    protected $helperData;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \BDA\CustomerRestriction\Helper\Data $helperData
+    )
+    {
+        $this->helperData = $helperData;
+    }
+    public function beforeGetErrorMessage(\MageKey\CustomerRestriction\Model\Restriction\Registration\ $subject, $title)
+    {
+        return $this->helperData->getGeneralConfig('error_message_registration');
+    }
+
+}
