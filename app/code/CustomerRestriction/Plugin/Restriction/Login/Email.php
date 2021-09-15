2e2b99e845db5f3a22864cd0b07ba5d061afdef0
Almir F Rivas Jr <rivasalmir@gmail.com>
2021-09-15 07:33:05 -0300

    Incluscao do modulo de restrição e mudança do plugin para after ao invés de before

---
+++ app/code/CustomerRestriction/Plugin/Restriction/Login/Email.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace BDA\CustomerRestriction\Plugin\Restriction\Login;
+
+class Email
+{
+
+    protected $helperData;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \BDA\CustomerRestriction\Helper\Data $helperData
+    )
+    {
+        $this->helperData = $helperData;
+    }
+    public function afterGetErrorMessage(\MageKey\CustomerRestriction\Model\Restriction\Registration\ $subject, $return)
+    {
+        return $this->helperData->getGeneralConfig('error_message_login');
+    }
+}
